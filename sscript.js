4221b8b95238d51104644770ffb5a36fbe260694
annazablah <74269619+annazablah@users.noreply.github.com>
2020-12-14 22:03:43 -0500

    Add files via upload

---
+++ sscript.js
@@ -0,0 +1,54 @@
+
+$(".box-item").mouseover(function () { 
+	var text= $(this).attr("data-esp")
+	$(this).find("h1").html(text)
+})
+
+$(".box-item").mouseout(function () {
+	var text= $(this).attr("data-eng")
+	$(this).find("h1").html(text)
+})
+
+
+
+
+
+$(".circle-item").mouseover(function () { 
+	var text= $(this).attr("data-esp")
+	$(this).find("h1").html(text)
+})
+
+$(".circle-item").mouseout(function () {
+	var text= $(this).attr("data-eng")
+	$(this).find("h1").html(text)
+})
+
+
+
+$(".shape-item").mouseover(function () { 
+	var text= $(this).attr("data-esp")
+	$(this).find("h1").html(text)
+})
+
+$(".shape-item").mouseout(function () {
+	var text= $(this).attr("data-eng")
+	$(this).find("h1").html(text)
+})
+
+
+
+
+
+$(".final-item").mouseover(function () { 
+	var text= $(this).attr("data-esp")
+	$(this).find("h1").html(text)
+})
+
+$(".final-item").mouseout(function () {
+	var text= $(this).attr("data-eng")
+	$(this).find("h1").html(text)
+})
+
+
+
+
